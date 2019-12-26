daf07f7f6e1bab29bb5894b02c79584df4bc5dbc
hamza <helkarou@gmail.com>
2019-12-26 14:39:54 +0100

    Add multi arch release script

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Simple URL Shortener in Scala and Play Framework
 
 ```shell script
 sh build.sh
-docker buildx build --platform linux/arm64 -t helkarou/twu --push -f dockerfile.aarch64 .
+docker buildx build --platform linux/arm64 -t helkarou/twu --push -f dockerfile.arm64v8 .
 ```
---
+++ release.sh
@@ -0,0 +1,46 @@
+name="teensy-weensy-url"
+organization="charik"
+version="1.0.0"
+
+repository="$organization/$name"
+
+echo "#################################################"
+echo "### BUILDING images for multiple architecture ###"
+echo "#################################################"
+
+
+for docker_arch in amd64 arm64v8; do
+  case ${docker_arch} in
+    amd64   ) baseimage_arch="openjdk" ;;
+    arm64v8 ) baseimage_arch="arm64v8/openjdk" ;;
+  esac
+
+  case ${docker_arch} in
+    amd64   ) platform_arch="linux/amd64" ;;
+    arm64v8 ) platform_arch="linux/arm64" ;;
+  esac
+
+  cp Dockerfile.cross Dockerfile.${docker_arch}
+  sed -i -- "s~__BASEIMAGE_ARCH__~$baseimage_arch~" Dockerfile.${docker_arch}
+
+  docker buildx build --platform ${platform_arch} -t ${repository}:${version}-${docker_arch} --push -f Dockerfile.${docker_arch} .
+done
+
+
+echo "#################################################"
+echo "###         Creating docker manifest          ###"
+echo "#################################################"
+
+manifest_args="$repository:$version"
+for docker_arch in amd64 arm64v8; do manifest_args=$manifest_args" $repository:${version}-${docker_arch} "; done;
+
+docker manifest create $manifest_args --amend
+
+for docker_arch in amd64 arm64v8; do
+    case ${docker_arch} in
+      arm64v8   ) docker manifest annotate ${repository}:${version} ${repository}:${version}-arm64v8 --os linux --arch arm64 --variant armv8  ;;
+      amd64     ) docker manifest annotate ${repository}:${version} ${repository}:${version}-amd64 --os linux --arch amd64 ;;
+    esac
+done
+
+docker manifest push ${repository}:${version}
